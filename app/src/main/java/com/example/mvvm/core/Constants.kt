a5c1240c2ac2b077e5df82fef97406d1850cb55c
Zachary Poudrier <2145093@cstj.qc.ca>
2023-02-02 11:33:15 -0500

    fin cours 4

---
+++ app/src/main/java/com/example/mvvm/core/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.mvvm.core
+
+object Constants {
+    const val EXPERIENCE_PER_LEVEL = 10
+}
---
+++ app/src/main/java/com/example/mvvm/domain/models/Pilot.kt
@@ -0,0 +1,35 @@
+package com.example.mvvm.domain.models
+
+import com.example.mvvm.core.Constants
+import kotlin.random.Random
+
+
+data class Pilot(var name: String, var life: Int, var cube: Int = 0) {
+
+        var shield = 10
+        var energie = 15
+
+        private var _experience: Int = 0;
+        val level: Int get() = _experience/ Constants.EXPERIENCE_PER_LEVEL
+
+        fun fly(revolutions: Int, isTraining: Boolean = false){
+                if(!isTraining){
+                        _experience += revolutions * Random.nextInt(1, 2 *level)
+                        life -= Random.nextInt(0,2)
+                        cube += Random.nextInt(0,2 *level)
+                }
+                energie -= Random.nextInt(1, 3)
+        }
+
+        fun canFly(): Boolean{
+                return life > 0 && energie >0
+        }
+
+        fun recharge(){
+                energie = Random.nextInt(0,10)
+                life = 10
+        }
+
+
+
+}
